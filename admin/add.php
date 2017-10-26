baf406fae74e4ad8ed70e3432831db96d2a30f75
naoki <manzisann@gmail.com>
2017-10-26 17:06:15 +0900

    Update admin css

--- admin/add.php
+++ admin/add.php
@@ -23,91 +23,33 @@
 <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
 <script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
-<script>
-$(function () {
-  var dateFormat = 'yy-mm-dd';
-  $('#datepicker').datepicker({
-      dateFormat: dateFormat
-  });
-
-  var myModel = {
-    friends: [
-    <?php for ($i=0; $i < $count; $i++) {
-        echo "{ name: \"$name[$i]\",},";
-       }
-      ?>
-    ],
-    search: ""
-
-  };
-
-  var myViewModel = new Vue({
-    el: '.form_employee',
-    data: myModel
-  });
-});
-</script>
 <title>来訪予定登録</title>
 </head>
 <body>
-<script type="text/javascript">
-function check(){
-
-  var flag = 0;
-
-  // 設定
-  if(document.form1.date.value == ""){ // 「お名前」の入力をチェック
-    flag = 1;
-  }
-  else if(document.form1.hours.value == ""){ // 「パスワード」の入力をチェック
-    flag = 1;
-  }
-  else if(document.form1.minutes.value == ""){ // 「コメント」の入力をチェック
-    flag = 1;
-  }
-  else if(document.form1.company.value == ""){ // 「コメント」の入力をチェック
-    flag = 1;
-  }
-  else if(document.form1.employee.value == ""){ // 「コメント」の入力をチェック
-    flag = 1;
-  }
-  // 設定終了
-
-  if(flag) {
-    alert('未入力の項目があります！'); // 入力漏れがあれば警告ダイアログを表示
-    return false; // 送信を中止
-  }
-  else {
-    return true; // 送信を実行
-  }
-
-}
-</script>
   <main>
-    <h1>アポイント登録</h1>
+    <h1>来訪予定登録</h1>
     <form method ="post" action="conf.php" name="form1" onSubmit="return check()">
-
       <div class="form_time">
-        <div class="label">日時</div>
+        <div class="input-label">日時</div>
         <div>
           <input type="text" class="form-control" name="date" id="datepicker" readonly>
-          <input type="number" class="time form-control" min="0" max="23" name="hours"><span>:</span>
-          <input type="number" class="time form-control" min="0" max="59" name="minutes"><span>〜</span>
+          <input type="text" class="time form-control" maxlength="2" name="hours"><span>:</span>
+          <input type="text" class="time form-control" maxlength="2" name="minutes"><span>〜</span>
         </div>
       </div>
 
       <div class="form_company">
-        <div class="label">来訪社名</div>
+        <div class="input-label">来訪社名</div>
         <input type="text" name="company" class="form-control">
       </div>
 
       <div class="form_customer">
-        <div class="label">来訪者氏名</div>
+        <div class="input-label">来訪者氏名</div>
         <input type="text" name="customer" class="form-control">様
       </div>
 
       <div class="form_employee">
-        <div class="label">担当者名</div>
+        <div class="input-label">担当者名</div>
         <select name="employee" class="form-control">
           <option v-for="friend in friends | filterBy search in 'name'" value="">
             {{ friend.name }}
@@ -142,6 +84,57 @@ function check(){
     option[i].value=id+1;
     id++;
   }
+  $(function () {
+  var dateFormat = 'yy-mm-dd';
+    $('#datepicker').datepicker({
+        dateFormat: dateFormat
+    });
 
+    var myModel = {
+      friends: [
+      <?php for ($i=0; $i < $count; $i++) {
+          echo "{ name: \"$name[$i]\",},";
+         }
+        ?>
+      ],
+      search: ""
+
+    };
+
+    var myViewModel = new Vue({
+      el: '.form_employee',
+      data: myModel
+    });
+  });
+  function check(){
+
+    var flag = 0;
+
+    // 設定
+    if(document.form1.date.value == ""){ // 「お名前」の入力をチェック
+      flag = 1;
+    }
+    else if(document.form1.hours.value == ""){ // 「パスワード」の入力をチェック
+      flag = 1;
+    }
+    else if(document.form1.minutes.value == ""){ // 「コメント」の入力をチェック
+      flag = 1;
+    }
+    else if(document.form1.company.value == ""){ // 「コメント」の入力をチェック
+      flag = 1;
+    }
+    else if(document.form1.employee.value == ""){ // 「コメント」の入力をチェック
+      flag = 1;
+    }
+    // 設定終了
+
+    if(flag) {
+      alert('未入力の項目があります！'); // 入力漏れがあれば警告ダイアログを表示
+      return false; // 送信を中止
+    }
+    else {
+      return true; // 送信を実行
+    }
+  }
 </script>
 </html>
--- admin/index.php
+++ admin/index.php
@@ -1,104 +1,104 @@
 <?php
-include("../dbconnection/config.php");
-include("../dbconnection/connect.php");
+  include("../dbconnection/config.php");
+  include("../dbconnection/connect.php");
 
-//今日の日付を取得する
+  //今日の日付を取得する
 
-$stmt = $pdo->prepare('select * from schedule order by hours asc, minutes asc;');
+  $stmt = $pdo->prepare('select * from schedule order by hours asc, minutes asc;');
 
 
-//実行
-$stmt -> execute();
-
+  //実行
+  $stmt -> execute();
 ?>
 
 <!doctype html>
 <html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <script src="https://code.jquery.com/jquery-3.2.0.js" integrity="sha256-wPFJNIFlVY49B+CuAIrDr932XSb6Jk3J1M22M3E2ylQ=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-    <script type="text/javascript">
-      $(function () {
-        var dateFormat = 'yy-mm-dd';
-        $('#datepicker').datepicker({
-          dateFormat: dateFormat
-        });
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="https://code.jquery.com/jquery-3.2.0.js" integrity="sha256-wPFJNIFlVY49B+CuAIrDr932XSb6Jk3J1M22M3E2ylQ=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+  <script scr="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.2/vue.js"></script>
+  <script type="text/javascript">
+    $(function () {
+      var dateFormat = 'yy-mm-dd';
+      $('#datepicker').datepicker({
+        dateFormat: dateFormat
       });
-    </script>
-    <title>来客受付システム</title>
-  </head>
-  <body>
-    <div id="wrapper">
-      <header>
-        <h1>来訪予定</h1>
-      </header>
-      <div id="refine">
-        <p>絞り込み</p>
-        <div id="dropdown">
-          <input type="text" class="form-control" id="datepicker" placeholder="日付" readonly>
-        </div>
-      <input type="text" class="form-control" placeholder="担当者名">
-      <div class="ctr_btn">
-        <button type="button" class="btn btn-success" onClick="location.href='add.php'">予定登録</button>
-        <button type="button" class="btn btn-danger" onClick="location.href='alldelete.php'">一括削除</button>
+    });
+  </script>
+  <title>来客受付システム</title>
+</head>
+<body>
+  <div id="wrapper">
+    <header>
+      <h1>来訪予定</h1>
+    </header>
+    <div id="refine">
+      <p>絞り込み</p>
+      <div id="dropdown">
+        <input type="text" class="form-control" id="datepicker" placeholder="日付" readonly>
       </div>
-      </div>
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th>選択</th>
-            <th>日付</th>
-            <th>会社名</th>
-            <th>来訪者名</th>
-            <th>担当者名</th>
-            <th>招待コード</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-        <?php
-          foreach ($stmt as $row):
-          $date = $row["date"];
-          $hours = $row["hours"];
-          $minutes = $row["minutes"];
-          $company = $row["company"];
-          $customer = $row["customer"];
-          $employee = $row["employee"];
-
-          $sql = $pdo -> prepare('SELECT name from employee where id = '.$employee);
-          $sql -> execute();
-          while($emp = $sql -> fetch(PDO::FETCH_ASSOC)){
-            $name=$emp["name"];
-          }
-          $employee = $name;
-          $code = $row["code"];
-          $id = $row["id"];
-        ?>
-        <tr>
-          <td><input type="checkbox" value="<?php echo $code ?>"></td>
-          <td><?php echo $date; ?> &nbsp; <?php echo $hours; ?>:<?php echo $minutes; ?></td>
-          <td><?php echo $company; ?></td>
-          <td><?php echo $customer; ?></td>
-          <td><?php echo $employee; ?></td>
-          <td><input type="text" readonly value="<?php echo $code; ?>"></td>
-          <td>
-            <form id="form" name="form" action="change.php" method="post">
-              <input type="hidden" name="id" value="<?php echo $id; ?>">
-              <button type="submit" class="btn btn-primary change">変更</button>
-            </form>
-            <form id="form" name="form" action="delete.php" method="post">
-              <input type="hidden" name="id" value="<?php echo $id; ?>">
-              <button type="submit" class="btn btn-danger delete">削除</button>
-            </form>
-          </td>
-        </tr>
-        <?php endforeach; ?>
-        </tbody>
-      </table>
+    <input type="text" class="form-control" placeholder="担当者名">
+    <div class="ctr_btn">
+      <button type="button" class="btn btn-success" onClick="location.href='add.php'">予定登録</button>
+      <button type="button" class="btn btn-danger" onClick="location.href='alldelete.php'">一括削除</button>
     </div>
-  </body>
+    </div>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>選択</th>
+          <th>日付</th>
+          <th>会社名</th>
+          <th>来訪者名</th>
+          <th>担当者名</th>
+          <th>招待コード</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+        foreach ($stmt as $row):
+        $date = $row["date"];
+        $hours = $row["hours"];
+        $minutes = $row["minutes"];
+        $company = $row["company"];
+        $customer = $row["customer"];
+        $employee = $row["employee"];
+
+        $sql = $pdo -> prepare('SELECT name from employee where id = '.$employee);
+        $sql -> execute();
+        while($emp = $sql -> fetch(PDO::FETCH_ASSOC)){
+          $name=$emp["name"];
+        }
+        $employee = $name;
+        $code = $row["code"];
+        $id = $row["id"];
+      ?>
+      <tr>
+        <td><input type="checkbox" value="<?php echo $code ?>"></td>
+        <td><?php echo $date; ?> <?php echo $hours; ?>:<?php echo $minutes; ?></td>
+        <td><?php echo $company; ?></td>
+        <td><?php echo $customer; ?></td>
+        <td><?php echo $employee; ?></td>
+        <td><input type="text" readonly value="<?php echo $code; ?>"></td>
+        <td>
+          <form id="form" name="form" action="change.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <button type="submit" class="btn btn-primary change">変更</button>
+          </form>
+          <form id="form" name="form" action="delete.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <button type="submit" class="btn btn-danger delete">削除</button>
+          </form>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
+</body>
 </html>
