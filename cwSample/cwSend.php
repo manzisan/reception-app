36e211c9ddd63eda9798069c2a49a9e8b7a2a3d5
naoki <manzisann@gmail.com>
2017-11-13 22:44:41 +0900

    Delete php mailer

--- cwSample/cwSend.php
+++ cwSample/cwSend.php
@@ -27,3 +27,26 @@
   $responce = curl_exec($ch);
   echo $responce;
   curl_close($ch);
+
+
+// //chatwork start
+//   //chatworkのルームID *rid以下* を指定する
+//   $roomId = "58637150"; //testルームIDが入ってる
+//   //APIKeyを指定する
+//   $api = "be450f970e02851abc3329c64e11011c";
+//       //投稿内容を入れる
+//       $body = '[info][title]来客通知[/title]来訪者がお見えになられてます。[/info]';
+
+//       $option = array('body' => $body);
+//       $ch = curl_init();
+//       curl_setopt($ch, CURLOPT_URL, 'https://api.chatwork.com/v1/rooms/' . $roomId . '/messages');
+//       curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-ChatWorkToken: ' . $api));
+//       curl_setopt($ch, CURLOPT_POST, 1);
+//       curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($option, '', '&'));
+
+//       //サーバー証明書の検証はしない
+//       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//       //投稿
+//       curl_exec($ch);
+//       curl_close($ch);
+//   //chatwork end
--- visited/view/sales.php
+++ visited/view/sales.php
@@ -3,26 +3,29 @@
   include_once "../layout/meta.php";
 ?>
 <body>
-<div id="wrapper">
-  <h1>会社名とお名前を入力してください</h1>
-    <form action="waitsale.php" method="post" id="form">
-      <p>会社名</p>
-      <input type="text" name="company" class="input">
-      <p>お名前</p>
-      <input type="text" name="name" class="input">
-      <p>どちらにご用ですか？</p>
-      <div class="button_list">
-        <button name = "require" value = "事務">事務</button>
-        <button name = "require" value = "経理">経理</button>
-        <button name = "require" value = "人事">人事</button>
-        <button name = "require" value = "その他">その他</button>
+  <div id="wrapper">
+    <h1>会社名とお名前を入力してください</h1>
+      <form action="waitsale.php" method="post" id="form">
+        <p>会社名</p>
+        <input type="text" name="company" class="input">
+        <p>お名前</p>
+        <input type="text" name="name" class="input">
+        <p>どちらにご用ですか？</p>
+        <div class="button_list">
+          <button>事務</button>
+          <button>経理</button>
+          <button>人事</button>
+          <button>その他</button>
+        </div>
+        <div>
+        <a href="waitdeli.php">配達業者様</a>
+        <a href="waitother.php">その他のお客様</a>
       </div>
-      <div>
-      <a href="waitdeli.php">配達業者様</a>
-      <a href="waitother.php">その他のお客様</a>
+    </form>
+    <div class="footer-btn-list">
+      <button onClick="location.href='recepcode.php'" class="back">戻る</button>
+      <button onclick="$('#form').submit();" class="next">次へ</button>
     </div>
-  </form>
-  <a href="index.php" class="back">戻る</a>
-</div>
+  </div>
 </body>
 </html>
